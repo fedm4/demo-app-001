a7fbe3f74099543334c1bfd930f618ed322d1774
Federico Marino <fede.m.fede@gmail.com>
2020-01-20 17:30:43 -0300

    Updates

---
+++ precache-manifest.b3de69b6826f67b3b6b8a8603bb4be53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de646f9856db9750e4546403cca8d3c8",
+    "url": "/demo-app-001/index.html"
+  },
+  {
+    "revision": "077f6346fa87bc2d3519",
+    "url": "/demo-app-001/static/css/main.e8014187.chunk.css"
+  },
+  {
+    "revision": "54a1d01046960887cd4c",
+    "url": "/demo-app-001/static/js/2.9b8ac162.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/demo-app-001/static/js/2.9b8ac162.chunk.js.LICENSE"
+  },
+  {
+    "revision": "077f6346fa87bc2d3519",
+    "url": "/demo-app-001/static/js/main.80de65a0.chunk.js"
+  },
+  {
+    "revision": "12c364cecdd4ee980e38",
+    "url": "/demo-app-001/static/js/runtime-main.e3c553db.js"
+  }
+]);
